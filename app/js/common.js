ab161e4167065944df5df761303672e14e27750c
BarryWin <43175007+BarryWin@users.noreply.github.com>
2019-04-03 21:04:19 +0300

    Made a navbar

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,34 @@
-$(document).ready({
+$(document).ready(function () {
+
+//rightMenu
+    var sideNav = $('#mySidenav');
+    $('.burger').click(function () {
+        $(this).toggleClass('burgerActive');
+        if (sideNav[0].style.width !== "350px") {
+            $('.overlay').fadeIn(500);
+            sideNav.get(0).style.width = "350px";
+            $('body, .upper-links').not('#mySidenav').css({
+                'paddingRight': "350px",
+            });
+            $('.burger')[0].style.marginRight = "350px";
+        } else {
+            $('.overlay').fadeOut(500);
+            sideNav[0].style.width = "0";
+            $('body, .upper-links').not('#mySidenav').css({
+                'paddingRight': "0",
+            });
+            $('.burger')[0].style.marginRight = "0";
+        }
+    });
+    $('.overlay').click(function () {
+        $('.overlay').fadeOut(500);
+        sideNav[0].style.width = "0";
+        $('body, .upper-links').not('#mySidenav').css({
+            'paddingRight': "0",
+        });
+        $('.burger')[0].style.marginRight = "0";
+        $('.burger').toggleClass('burgerActive');
+    })
 
 });
+
